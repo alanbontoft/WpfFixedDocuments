f8659e394e02399ad3582a8dfea522e6efd84aff
alanbonoft <alan.bonoft@gmail.com>
2023-11-07 20:48:17 +0000

    Initial commit

---
+++ WpfFixedDoc/AppModel.cs
@@ -0,0 +1,270 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.IO.Packaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows.Xps.Packaging;
+using System.Reflection.Metadata;
+using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Markup;
+using System.Windows.Media.Imaging;
+using CommunityToolkit.Mvvm.DependencyInjection;
+using System.Xml.Linq;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace WpfFixedDoc
+{
+    public partial class AppModel : ObservableObject
+    {
+
+        private XpsDocument _xpsPackage;
+
+        private string _xpsFilename = "c:\\temp\\test.xps";
+
+        [ObservableProperty]
+        private IDocumentPaginatorSource _liveDoc;
+
+        [ObservableProperty]
+        private int _counter;
+
+        [RelayCommand]
+        private void OpenFile()
+        {
+            using (var fd = new CommonOpenFileDialog())
+            {
+                if (fd.ShowDialog() == CommonFileDialogResult.Ok)
+                {
+                    _xpsPackage = new XpsDocument(fd.FileName, FileAccess.Read, CompressionOption.NotCompressed);
+
+                    // Get the FixedDocumentSequence from the package.
+                    FixedDocumentSequence fixedDocumentSequence = _xpsPackage.GetFixedDocumentSequence();
+
+                    // Set the new FixedDocumentSequence as
+                    // the DocumentViewer's paginator source.
+                    LiveDoc = fixedDocumentSequence as IDocumentPaginatorSource;
+                }
+            }
+
+
+        }
+
+        [RelayCommand]
+        private void CreateDoc()
+        {
+            LiveDoc = createFixedDoc();
+        }
+
+        [RelayCommand]
+        private void PrintDoc()
+        {
+            var pd = new PrintDialog() { PageRangeSelection = PageRangeSelection.UserPages };
+
+            if (pd.ShowDialog() == true)
+            {
+                pd.PrintDocument(LiveDoc.DocumentPaginator, "Print Job Name");
+            }
+        }
+
+        [RelayCommand]
+        private void SaveXps()
+        {
+            var fd = new CommonOpenFileDialog() { IsFolderPicker = true };
+
+            if (fd.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                saveXpsFile(Path.Combine(fd.FileName, "test.xps"));
+            }
+        }
+
+        [RelayCommand]
+        private void SavePdf()
+        {
+            var fd = new CommonOpenFileDialog() { IsFolderPicker = true };
+
+            if (fd.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                savePdfFile(Path.Combine(fd.FileName, "test.pdf"));
+            }
+        }
+
+        private FixedDocument createFixedDoc()
+        {
+
+            var document = new FixedDocument();
+
+            document.DocumentPaginator.PageSize = new System.Windows.Size(793.7, 1122.5);
+
+            var margin = new Thickness(50);
+
+            // create a page
+            FixedPage page1 = new FixedPage();
+            page1.Width = document.DocumentPaginator.PageSize.Width;
+            page1.Height = document.DocumentPaginator.PageSize.Height;
+
+            var panel = new StackPanel() { Width = page1.Width }; //, Height = page1.Height };
+
+            var logo = new System.Windows.Controls.Image();
+
+            logo.Source = createBitmap("fish");
+            logo.Height = 150;
+            logo.HorizontalAlignment = HorizontalAlignment.Left;
+            logo.Margin = new Thickness(margin.Left, margin.Top / 2, margin.Right, margin.Bottom / 2);
+
+            panel.Children.Add(logo);
+
+            // panel.Orientation = Orientation.Vertical;
+
+            // add title to the page
+            var page1Title = new TextBlock();
+            page1Title.Text = "This is the first page";
+            page1Title.FontSize = 40; // 30pt text
+            page1Title.Margin = margin; // 1 inch margin
+            // page1.Children.Add(page1Title);
+
+            DockPanel.SetDock(page1Title, Dock.Top);
+
+            // panel.Children.Add(page1Title);
+
+
+            // add subtitle to the page
+            var page1Subtitle = new TextBlock();
+            page1Subtitle.Text = "This is also the first page";
+            page1Subtitle.FontSize = 30; // 30pt text
+            page1Subtitle.Margin = margin; // 1 inch margin
+
+            //  panel.Children.Add(page1Subtitle);
+
+
+            var grid = new Grid();
+
+            RowDefinition row_def;
+            ColumnDefinition col_def;
+
+            var gridBrush = new SolidColorBrush(System.Windows.Media.Color.FromRgb(0, 255, 0));
+
+            gridBrush = Brushes.Black;
+
+            for (int i = 0; i < 3; i++)
+            {
+                row_def = new RowDefinition();
+                grid.RowDefinitions.Add(row_def);
+
+                col_def = new ColumnDefinition();
+
+                if (i == 1) col_def.Width = new GridLength(0.5, GridUnitType.Star);
+
+                grid.ColumnDefinitions.Add(col_def);
+
+            }
+
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                {
+                    var txt = new TextBlock { Text = $"{i},{j}", HorizontalAlignment = HorizontalAlignment.Center };
+
+
+                    var border = new Border();
+
+                    border.BorderBrush = gridBrush;
+
+                    border.BorderThickness = new Thickness(1);
+
+                    border.Child = txt;
+
+                    Grid.SetRow(border, i);
+                    Grid.SetColumn(border, j);
+
+                    txt.Foreground = new SolidColorBrush(System.Windows.Media.Color.FromRgb(255, 0, 0));
+                    txt.Foreground = Brushes.Black;
+                    txt.Margin = new Thickness(5);
+                    grid.Children.Add(border);
+                }
+
+            // DockPanel.SetDock(grid, Dock.Bottom);
+
+            var outerBorder = new Border();
+
+            outerBorder.BorderBrush = gridBrush;
+
+            outerBorder.BorderThickness = new Thickness(2);
+
+            outerBorder.Child = grid;
+
+
+            outerBorder.Margin = new Thickness(margin.Left, 0, margin.Right, 0);
+
+            panel.Children.Add(outerBorder);
+
+
+            page1.Children.Add(panel);
+
+            // add the page to the document
+            PageContent page1Content = new PageContent();
+            ((IAddChild)page1Content).AddChild(page1);
+            document.Pages.Add(page1Content);
+
+            // do the same for the second page
+            FixedPage page2 = new FixedPage();
+            page2.Width = document.DocumentPaginator.PageSize.Width;
+            page2.Height = document.DocumentPaginator.PageSize.Height;
+            TextBlock page2Text = new TextBlock();
+            page2Text.Text = "This is NOT the first page";
+            page2Text.FontSize = 40;
+            page2Text.Margin = new Thickness(96);
+            page2.Children.Add(page2Text);
+            PageContent page2Content = new PageContent();
+            ((IAddChild)page2Content).AddChild(page2);
+
+            //document.Pages.Add(page2Content);
+
+            return document;
+        }
+
+        private BitmapImage createBitmap(string imagename)
+        {
+            var name = $"pack://application:,,/images/{imagename}.png";
+
+            var image = new BitmapImage();
+            image.BeginInit();
+            image.UriSource = new Uri(name, UriKind.RelativeOrAbsolute);
+            image.EndInit();
+
+            return image;
+        }
+
+        private bool saveXpsFile(string filename)
+        {
+            var result = true;
+
+            try
+            {
+                var xps = new XpsDocument(filename, FileAccess.Write, CompressionOption.NotCompressed);
+
+                var writer = XpsDocument.CreateXpsDocumentWriter(xps);
+                writer.Write(LiveDoc as FixedDocument);
+
+                xps.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                result = false;
+            }
+
+            return result;
+        }
+
+        private void savePdfFile(string filename)
+        {
+            // Print to PDF
+            PdfFilePrinter.PrintXpsToPdf(LiveDoc as FixedDocument, filename, "Document Title");
+        }
+    }
+
+}
---
+++ WpfFixedDoc/PdfFilePrinter.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Linq;
+using System.Printing;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Windows.Documents;
+using System.IO.Packaging;
+using System.Windows.Xps.Packaging;
+
+public static class PdfFilePrinter
+{
+    private const string PdfPrinterDriveName = "Microsoft Print To PDF";
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+    private class DOCINFOA
+    {
+        [MarshalAs(UnmanagedType.LPStr)]
+        public string pDocName;
+        [MarshalAs(UnmanagedType.LPStr)]
+        public string pOutputFile;
+        [MarshalAs(UnmanagedType.LPStr)]
+        public string pDataType;
+    }
+
+    [DllImport("winspool.drv", EntryPoint = "OpenPrinterA", SetLastError = true, CharSet = CharSet.Ansi, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
+    private static extern bool OpenPrinter([MarshalAs(UnmanagedType.LPStr)] string szPrinter, out IntPtr hPrinter, IntPtr pd);
+
+    [DllImport("winspool.drv", EntryPoint = "ClosePrinter", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
+    private static extern bool ClosePrinter(IntPtr hPrinter);
+
+    [DllImport("winspool.drv", EntryPoint = "StartDocPrinterA", SetLastError = true, CharSet = CharSet.Ansi, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
+    private static extern int StartDocPrinter(IntPtr hPrinter, int level, [In, MarshalAs(UnmanagedType.LPStruct)] DOCINFOA di);
+
+    [DllImport("winspool.drv", EntryPoint = "EndDocPrinter", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
+    private static extern bool EndDocPrinter(IntPtr hPrinter);
+
+    [DllImport("winspool.drv", EntryPoint = "StartPagePrinter", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
+    private static extern bool StartPagePrinter(IntPtr hPrinter);
+
+    [DllImport("winspool.drv", EntryPoint = "EndPagePrinter", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
+    private static extern bool EndPagePrinter(IntPtr hPrinter);
+
+    [DllImport("winspool.drv", EntryPoint = "WritePrinter", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
+    private static extern bool WritePrinter(IntPtr hPrinter, IntPtr pBytes, int dwCount, out int dwWritten);
+
+    
+    /// <summary>
+    /// Print an XPS document to a 
+    /// </summary>
+    /// <param name="bytes"></param>
+    /// <param name="outputFilePath"></param>
+    /// <param name="documentTitle"></param>
+    /// <exception cref="Exception"></exception>
+	public static void PrintXpsToPdf(FixedDocument doc, string outputFilePath, string documentTitle)
+    {
+        var bytes = FixedDocumentToBytes(doc);
+
+        // Get Microsoft Print to PDF print queue
+        var pdfPrintQueue = GetMicrosoftPdfPrintQueue();
+
+        // Copy byte array to unmanaged pointer
+        var ptrUnmanagedBytes = Marshal.AllocCoTaskMem(bytes.Length);
+        Marshal.Copy(bytes, 0, ptrUnmanagedBytes, bytes.Length);
+
+        // Prepare document info
+        var di = new DOCINFOA
+        {
+            pDocName = documentTitle,
+            pOutputFile = outputFilePath,
+            pDataType = "RAW"
+        };
+
+        // Print to PDF
+        var errorCode = SendBytesToPrinter(pdfPrintQueue.Name, ptrUnmanagedBytes, bytes.Length, di, out var jobId);
+
+        // Free unmanaged memory
+        Marshal.FreeCoTaskMem(ptrUnmanagedBytes);
+
+        // Check if job in error state (for example not enough disk space)
+        var jobFailed = false;
+        try
+        {
+            var pdfPrintJob = pdfPrintQueue.GetJob(jobId);
+            if (pdfPrintJob.IsInError)
+            {
+                jobFailed = true;
+                pdfPrintJob.Cancel();
+            }
+        }
+        catch
+        {
+            // If job succeeds, GetJob will throw an exception. Ignore it. 
+        }
+        finally
+        {
+            pdfPrintQueue.Dispose();
+        }
+
+        if (errorCode > 0 || jobFailed)
+        {
+            try
+            {
+                if (File.Exists(outputFilePath))
+                {
+                    File.Delete(outputFilePath);
+                }
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+
+        if (errorCode > 0)
+        {
+            throw new Exception($"Printing to PDF failed. Error code: {errorCode}.");
+        }
+
+        if (jobFailed)
+        {
+            throw new Exception("PDF Print job failed.");
+        }
+    }
+
+    private static int SendBytesToPrinter(string szPrinterName, IntPtr pBytes, int dwCount, DOCINFOA documentInfo, out int jobId)
+    {
+        jobId = 0;
+        var dwWritten = 0;
+        var success = false;
+
+        if (OpenPrinter(szPrinterName.Normalize(), out var hPrinter, IntPtr.Zero))
+        {
+            jobId = StartDocPrinter(hPrinter, 1, documentInfo);
+            if (jobId > 0)
+            {
+                if (StartPagePrinter(hPrinter))
+                {
+                    success = WritePrinter(hPrinter, pBytes, dwCount, out dwWritten);
+                    EndPagePrinter(hPrinter);
+                }
+
+                EndDocPrinter(hPrinter);
+            }
+
+            ClosePrinter(hPrinter);
+        }
+
+        // TODO: The other methods such as OpenPrinter also have return values. Check those?
+
+        if (success == false)
+        {
+            return Marshal.GetLastWin32Error();
+        }
+
+        return 0;
+    }
+
+    private static PrintQueue GetMicrosoftPdfPrintQueue()
+    {
+        PrintQueue pdfPrintQueue = null;
+
+        try
+        {
+            using (var printServer = new PrintServer())
+            {
+                var flags = new[] { EnumeratedPrintQueueTypes.Local };
+                // FirstOrDefault because it's possible for there to be multiple PDF printers with the same driver name (though unusual)
+                // To get a specific printer, search by FullName property instead (note that in Windows, queue name can be changed)
+                pdfPrintQueue = printServer.GetPrintQueues(flags).FirstOrDefault(lq => lq.QueueDriver.Name == PdfPrinterDriveName);
+            }
+
+            if (pdfPrintQueue == null)
+            {
+                throw new Exception($"Could not find printer with driver name: {PdfPrinterDriveName}");
+            }
+
+            if (!pdfPrintQueue.IsXpsDevice)
+            {
+                throw new Exception($"PrintQueue '{pdfPrintQueue.Name}' does not understand XPS page description language.");
+            }
+
+            return pdfPrintQueue;
+        }
+        catch
+        {
+            pdfPrintQueue?.Dispose();
+            throw;
+        }
+    }
+
+    private static byte[] FixedDocumentToBytes(FixedDocument fixeddoc)
+    {
+        // Convert FixedDocument to XPS file in memory
+        var ms = new MemoryStream();
+        var package = Package.Open(ms, FileMode.Create);
+        var xpsdoc = new XpsDocument(package);
+        var writer = XpsDocument.CreateXpsDocumentWriter(xpsdoc);
+        writer.Write(fixeddoc.DocumentPaginator);
+        xpsdoc.Close();
+        package.Close();
+
+        // Get XPS file bytes
+        var bytes = ms.ToArray();
+        ms.Dispose();
+
+        return bytes;
+    }
+}
